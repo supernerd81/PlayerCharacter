7df891bf238417e6cec93a32993146f419227a86
Manuel Simon <info@der-supernerd.de>
2025-01-30 16:48:53 +0100

    Playercharacter onKeyPressed X Plus One and Test implementation

--- src/main/java/PlayerCharacter.java
+++ src/main/java/PlayerCharacter.java
@@ -1,9 +1,44 @@
 public class PlayerCharacter {
+    public static int positionX = 0;
+    public static int positionY = 0;
+
     public static int getX() {
-        return 0;
+        return PlayerCharacter.positionX;
     }
 
     public static int getY() {
-        return 0;
+        return PlayerCharacter.positionY;
+    }
+
+    public static int move(char arg) {
+
+        int returns;
+
+        switch(arg) {
+            case 'W':
+                positionY++;
+                returns = getY();
+                break;
+
+            case 'S':
+                positionY--;
+                returns = getY();
+                break;
+
+            case 'D':
+                positionX++;
+                returns = getX();
+                break;
+
+            case 'A':
+                positionX--;
+                returns = getX();
+                break;
+
+            default:
+                returns = 42;
+        }
+
+        return returns;
     }
 }
--- src/test/java/PlayerCharacterTest.java
+++ src/test/java/PlayerCharacterTest.java
@@ -1,7 +1,15 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PlayerCharacterTest {
+
+
+    @BeforeEach
+    void resetAllXY() {
+        PlayerCharacter.positionX = 0;
+        PlayerCharacter.positionY = 0;
+    }
 
     @Test
     void getX_null() {
@@ -14,6 +22,15 @@ public class PlayerCharacterTest {
     void getY_null() {
         int expect = 0;
         int actual = PlayerCharacter.getY();
+        Assertions.assertEquals(expect, actual);
+    }
+
+    @Test
+    void movedUp_onPressed_key_W_VarX_PlusOne() {
+        char given = 'W';
+        int expect = 1;
+        int actual = PlayerCharacter.move(given);
+
         Assertions.assertEquals(expect, actual);
     }
 }
